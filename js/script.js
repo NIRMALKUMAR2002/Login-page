8a478d83e0ea2495b0b2cd055ded716ffdd757f3
NIRMALKUMAR2002 <nirmalkumar07112002@gmail.com>
2024-04-05 23:12:47 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//  const a = document.getElementsById("login")
+//  a.addEventListener("mouseover",function success(){
+ 
+//      window.alert("login successfully")
+//  });
+const a =document.getElementById("login")
+// function success(){
+//     window.alert();  
+                     
+// }
+a.onclick = ()=>{
+    window.alert("login");
+}
